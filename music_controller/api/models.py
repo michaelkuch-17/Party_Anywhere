593585c426ea2397196e95c1580b30b3f2ad961f
michaelkuch-17 <z1814996@students.niu.edu>
2021-01-02 11:50:44 -0600

    rooms can not be made. V0.2 video 2 completed

--- music_controller/api/models.py
+++ music_controller/api/models.py
@@ -20,4 +20,4 @@ class Room(models.Model):
     host = models.CharField(max_length = 50, unique = True)
     guest_can_pause = models.BooleanField(null = False, default = False)
     votes_to_skip = models.IntegerField(null = False, default = 1)
-    create_at = models.DateField(auto_now_add = True)
+    created_at = models.DateField(auto_now_add = True)
