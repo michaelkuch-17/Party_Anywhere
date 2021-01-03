5ef64077f9fd8e864fb456e613549be026c26252
michaelkuch-17 <z1814996@students.niu.edu>
2021-01-02 18:07:59 -0600

    v0.5 completed. create-room get/post working

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RoomView
+from .views import RoomView, RoomCreateView
 
 urlpatterns = [
     path('room', RoomView.as_view()),
+    path('create-room', RoomCreateView.as_view())
 ]
