b2ca95921f8c853763278c8029024fe88626b159
michaelkuch-17 <z1814996@students.niu.edu>
2021-01-04 12:07:51 -0600

    video 7 finished, get errors resolved

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,22 +1,30 @@
-import React, { Component } from 'react';
-import RoomJoinPage from"./RoomJoinPage";
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
 import RoomCreatePage from "./RoomCreatePage";
 import Room from "./Room";
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 export default class HomePage extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render(){
-        return <Router>
-            <Switch>
-                <Route exact path="/"><p>This is the home page!</p></Route>
-                <Route path="/join" component={RoomJoinPage}/>
-                <Route path="/create-room" component={RoomCreatePage}/>
-                <Route path="/room/:roomCode" component={Room} />
-            </Switch>
-        </Router>;
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/"><p>This is the home page</p></Route>
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create-room" component={RoomCreatePage} />
+          <Route path="/room/:roomCode" component={Room} />
+        </Switch>
+      </Router>
+    );
+  }
 }
