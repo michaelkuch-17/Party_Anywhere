3dff72a3001b329bb4cef89eeb1f1ad99990aba9
michaelkuch-17 <z1814996@students.niu.edu>
2021-01-04 17:32:24 -0600

    finished videos 9 and 10

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RoomView, RoomCreateView, GetRoom, RoomJoin
+from .views import RoomView, RoomCreateView, GetRoom, RoomJoin, UserInRoom, RoomLeave
 
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', RoomCreateView.as_view()),
     path('get-room', GetRoom.as_view()),
-    path('join-room', RoomJoin.as_view())
+    path('join-room', RoomJoin.as_view()),
+    path('UIR', UserInRoom.as_view()),
+    path('leave-room', RoomLeave.as_view()),
 ]
--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import RoomCreatePage from "./RoomCreatePage";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core"
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,16 +14,74 @@ import {
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
+    this.state  = {
+        roomCode: null,
+    };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
+
+
+  async componentDidMount() {
+    fetch("/api/UIR")
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                roomCode: data.code
+            });
+        });
+  }
+
+
+  renderHomePage() {
+      return (
+        <Grid container spacing={3}>
+            <Grid item xs={12} align="center">
+                <Typography variant="h3" compact="h3">
+                    Party Anywhere
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <ButtonGroup disableElevation variant="contained" color="primary">
+                    <Button color="primary" to='/join' component={Link}>
+                        Join a Room
+                    </Button>
+                    <Button color="secondary" to='/create-room' component={Link}>
+                        Create a Room
+                    </Button>
+                </ButtonGroup>
+            </Grid>
+        </Grid>
+      );
+  }
+
+
+  clearRoomCode() {
+      this.setState({
+          roomCode: null,
+      });
+  }
+
 
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/"><p>This is the home page</p></Route>
+          <Route exact path="/" render={() => {
+              return this.state.roomCode ? 
+                (<Redirect to={`/room/${this.state.roomCode}`}/>
+              ) : (
+                  this.renderHomePage()
+              );
+            }}
+          />
           <Route path="/join" component={RoomJoinPage} />
           <Route path="/create-room" component={RoomCreatePage} />
-          <Route path="/room/:roomCode" component={Room} />
+          <Route
+            path="/room/:roomCode"
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+            }}
+          />
         </Switch>
       </Router>
     );
